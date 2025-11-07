d4858828e36aa394a09020e3435959f0144422aa
bagas bgs <bagasbgs2516@gmail.com>
2025-11-07 16:57:57 +0700

    update 2

---
+++ src/app/Classroom/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function ClassroomCapitalRedirectPage() {
+  redirect('https://us06web.zoom.us/j/4805595450?pwd=XTAPk0zNuhEBDXUhMKpWTmrKLhB7pc.1');
+}
